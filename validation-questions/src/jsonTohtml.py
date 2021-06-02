825e15d346aea05d677856af21a7d527e3de2f79
mpuerta004 <mpuerta004@hotmail.com>
2021-06-02 17:28:10 +0200

    Update jsonTohtml.py

--- validation-questions/src/jsonTohtml.py
+++ validation-questions/src/jsonTohtml.py
@@ -6,27 +6,29 @@ folder_jsons = sys.argv[1]
 fordel_html = sys.argv[2]
 
 files = listdir(folder_jsons)
+# unicamente nos interesan los archivos .json
 json_files = []
 for i in files:
     if i.endswith(".json"):
         json_files.append(i)
 files = [folder_jsons  + '/' + f for f in json_files]
-print(files)
 
 for f in files:
+    # Obtenemos el string del fichero json y eliminados el fichero json. 
     json_result = open(f, 'r')
     json_string=json_result.read()
     json_result.close()
     remove(f)
 
+    # pasamos el string a html.
     json_in_html = json2html.convert(json=json_string)
-    print(json_in_html)
+    # Obtemeos el nombre de la pregunta esparql par apoder denominar a nuestro dichero html con el mismo nombre 
     index_start = f.index('Q')
     index_end = f.index('.')
     id_query_string = f[index_start:index_end]
 
-    string_destino = fordel_html + '/' + id_query_string + '.html'
-    print(string_destino)
-    file_html = open(string_destino, "w")
-    file_html.write(json_in_html)
+    
+    string_destino = fordel_html + '/' + id_query_string + '.html' 
+    file_html = open(string_destino, "w") 
+    file_html.write(json_in_html) 
     file_html.close()
