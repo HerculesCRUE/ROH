2d902c90bf0eb9c5cd154c3ae22aec497ab5e05a
mpuerta004 <mpuerta004@hotmail.com>
2021-06-09 09:54:39 +0200

    Update jsonTohtml.py

--- validation-questions/src/jsonTohtml.py
+++ validation-questions/src/jsonTohtml.py
@@ -5,6 +5,7 @@ from os import listdir
 folder_jsons = sys.argv[1]
 fordel_html = sys.argv[2]
 
+
 prefix = open('prefix.json', 'r')
 content = prefix.read()
 dic_of_prefix = json.loads(content)
@@ -44,7 +45,8 @@ for f in files:
         pass
     columns = []
     List_of_string_of_prefix = [
-        '<head><style> html { font-family: sans-serif;}details > p {font-family: "Courier New"; padding: 4px;  background-color: #eeeeee;  border: none;  box-shadow: 1px 1px 2px #bbbbbb;  cursor: pointer;}p {   text-align: justify;  letter-spacing: 1px;  font-size: 0.9rem; }table {border-collapse: collapse;  border: 2px solid rgb(200,200,200); letter-spacing: 1px;  font-size: 0.8rem;}td, th {border: 1px solid rgb(190,190,190);  padding: 10px 20px;}th {  background-color: rgb(235,235,235);}td {  text-align: center;}tr:nth-child(even) td {  background-color: rgb(250,250,250);}tr:nth-child(odd) td {background-color: rgb(245,245,245);}caption { padding: 10px;}</style></head>']
+        '<head><style> html { font-family: sans-serif;}p{width:85%} div {display: inline-block; width:85%;text-align:center;}div >left {display: inline-block;float:left;width:85px;}div > center {display: inline-block;width:100px;}div > right {display: inline-block;float:right;width:100px;}details > p {font-family: "Courier New"; padding: 4px;  background-color: #eeeeee;  border: none;  box-shadow: 1px 1px 2px #bbbbbb;  cursor: pointer;}p {   text-align: justify;  letter-spacing: 1px;  font-size: 0.9rem; }table {border-collapse: collapse;  border: 2px solid rgb(200,200,200); letter-spacing: 1px;  font-size: 0.8rem;}td, th {border: 1px solid rgb(190,190,190);  padding: 10px 20px;}th {  background-color: rgb(235,235,235);}td {  text-align: center;}tr:nth-child(even) td {  background-color: rgb(250,250,250);}tr:nth-child(odd) td {background-color: rgb(245,245,245);}caption { padding: 10px;}</style></head>']
+
 
     try:
         for i in jsondecoded[0].keys():
@@ -102,16 +104,16 @@ for f in files:
     cadena = ''
     for i in List_of_string_of_prefix:
         cadena = cadena + i
-    
-    json_in_html = cadena + json_in_html
 
+    json_in_html = cadena + json_in_html
     string='</br><div style = "width:82%" >'
     string=string+'<right > <a href="https://github.com/HerculesCRUE/ROH"> Repositorio&nbspPintcipal.</a> </right >'
     string=string + '<center  > <a href="hhttps://github.com/HerculesCRUE/ROH/blob/main/docs/2-%20CoberturaPreguntasCompetencia.pdf"> CoberturaPreguntasCompetencia.pdf.</a> </center>'
     string = string + '<left > <a href="https://github.com/HerculesCRUE/ROH/blob/main/validation-questions/sparql-query/README.md"> Preguntas&nbspSparql&nbspy&nbspresults.</a> </left>'
     string =string +'</div>'
-   
+
     json_in_html=json_in_html+string
+
 
     # pasamos el string a html.
 
